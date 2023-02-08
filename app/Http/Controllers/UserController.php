003db4ffb6cda2a7fb26cb7a6f7fb0658072afde
tranthanhuet <tranthanhuet@gmail.com>
2023-02-08 17:42:33 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+use Hash;
+class UserController extends Controller
+{
+    //
+    protected function isAdmin(User $user){
+        if($user->role == 'admin'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    protected function index(){
+        $user = User::find(auth()->user()->id);
+        if(!$this->isAdmin($user)){
+            return response()->json([
+                'status_code' => 401,
+                'message' => 'Unauthorized'
+            ]);
+        }
+        return response()->json([
+            'data' => User::all(),
+        ]);
+    }
+    protected function create(Request $request){
+        $user = User::find(auth()->user()->id);
+        if(!$this->isAdmin($user)){
+            return response()->json([
+                'status_code' => 401,
+                'message' => 'Unauthorized'
+            ]);
+        }
+
+        $request->validate([
+            'email' => 'email|required',
+            'role' => 'required',
+            'name' => 'required',
+        ]);
+
+        $new_user = request(['email', 'name', 'phone', 'role']);
+        $password = 123456;
+        $new_user['password'] = Hash::make($password);
+        $user = User::create($new_user);
+        // $password = rand(100000,999999);
+        
+        $user->save();
+        return response()->json($user);
+
+        //Send Email
+
+        
+    }
+    
+
+    protected function login(Request $request){
+        try {
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'required'
+            ]);
+
+            $credentials = request(['email', 'password']);
+            
+            
+            if (!Auth::attempt($credentials)) {
+                return response()->json([
+                    'status_code' => 500,
+                    'message' => 'Unauthorized'
+                ]);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            if(!$user->active){
+                return response()->json([
+                    'status_code' => 500,
+                    'message' => 'User deactivated'
+                ]);
+            }
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Error in Login');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json([
+                'status_code' => 200,
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+            ]);
+        } catch (\Exception $error) {
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Error in Login',
+                'error' => $error,
+            ]);
+        }
+    }
+    
+    protected function update(Request $request){
+        $request->validate([
+            // 'phone' => 'email|required',
+            'id' => 'required',
+        ]);
+        $user = User::find(auth()->user()->id);
+        if(!$this->isAdmin($user)){
+            return response()->json([
+                'status_code' => 401,
+                'message' => 'Unauthorized'
+            ]);
+        }
+        $user = User::find($request->id);
+        $update = request(['email', 'fullname', 'role', 'phone']);
+        if(!$user){
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Unauthorized'
+            ]); 
+        }
+        if(!$this->isAdmin($user)){
+            return response()->json([
+                'status_code' => 401,
+                'message' => 'Unauthorized'
+            ]);
+        }
+        $user->update($update);
+        return response()->json([
+            'status_code' => 200,
+        ]); 
+
+    }
+    protected function deactive(Request $request){
+        $request->validate([
+            // 'phone' => 'email|required',
+            'id' => 'required',
+        ]);
+        $user = User::find(auth()->user()->id);
+        if(!$this->isAdmin($user)){
+            return response()->json([
+                'status_code' => 401,
+                'message' => 'Unauthorized'
+            ]);
+        }
+        $user = User::find($request->id);
+        if(!$user){
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Unauthorized'
+            ]); 
+        }
+        
+        $user->active = 0;
+        $user->save();
+
+    }
+    protected function reactive(Request $request){
+        $request->validate([
+            // 'phone' => 'email|required',
+            'id' => 'required',
+        ]);
+        $user = User::find(auth()->user()->id);
+        if(!$this->isAdmin($user)){
+            return response()->json([
+                'status_code' => 401,
+                'message' => 'Unauthorized'
+            ]);
+        }
+        $user = User::find($request->id);
+        if(!$user){
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Unauthorized'
+            ]); 
+        }
+        
+        $user->active = 1;
+        $user->save();
+
+    }
+    protected function resetPassword(Request $request){
+        $request->validate([
+            // 'phone' => 'email|required',
+            'id' => 'required',
+        ]);
+        $user = User::find(auth()->user()->id);
+        if(!$this->isAdmin($user)){
+            return response()->json([
+                'status_code' => 401,
+                'message' => 'Unauthorized'
+            ]);
+        }
+        $user = User::find($request->id);
+        if(!$user){
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Unauthorized'
+            ]); 
+        }
+        
+        $user->password = Hash::make('123456');
+        $user->save();
+
+    }
+    
+
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::post('/login', [UserController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users/update', [UserController::class, 'update']);
+    Route::post('/users/create', [UserController::class, 'create']);
+    Route::post('/users/deactive', [UserController::class, 'deactive']);
+    Route::post('/users/reactive', [UserController::class, 'reactive']);
+    Route::post('/users/reset-password', [UserController::class, 'resetPassword']);
+
+    // Route::post('/users/create')
+});
